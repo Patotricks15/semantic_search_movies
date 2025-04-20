bee0cac2cca4edeb196df2d640ae6cba1f38ed0a
Patotricks <patrickufrrj@gmail.com>
2025-04-20 15:14:54 -0300

    Adding the neobrutalism aesthetic

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,21 @@
 import streamlit as st
 from generate_vectors import MoviesVectorDataBase
+import os
 
 db = MoviesVectorDataBase()
 db.generate_vectors()
+
+# Function to load a CSS file and inject its content into the page
+def local_css(file_name):
+    with open(file_name, "r", encoding="utf-8") as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# Load CSS files
+css_path = os.path.join("css", "general.css")
+local_css(css_path)
+
+css_path = os.path.join("css", "components.css")
+local_css(css_path)
 
 def get_recommended_movies(search: str, n: int = 3):
     message = ''
